cc621ffb23c49f0991595006282aabc12adc8475
agamt1511 <at1511@bs.amalnet.k12.il>
2022-10-08 09:51:08 +0300

    Initial commit

---
+++ app/src/main/java/com/example/alphaversion1/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.alphaversion1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private EditText email, password;
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        btn = (Button) findViewById(R.id.btn);
+    }
+
+    public void register(View view) {
+        String addEmail = email.getText().toString().trim();
+        String addPassword = password.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(addEmail,addPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    User user = new User(addEmail,addPassword);
+
+                    FirebaseDatabase.getInstance().getReference("User")
+                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                            .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()){
+                                        Toast.makeText(MainActivity.this, "User added", Toast.LENGTH_LONG).show();
+                                    }
+                                    else {
+                                        Toast.makeText(MainActivity.this, "User not added", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+    }
+}
